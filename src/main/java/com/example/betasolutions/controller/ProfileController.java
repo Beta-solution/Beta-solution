cc779db7e2cb72affdd14e8105b64bc9c687322b
MagnusNH <Magn763j@stud.ek.dk>
2026-05-07 16:14:13 +0200

    En lidt rodet omgang: - Role enum klasse: Admin har skiftet navn til owner. - Exceptions er lavet. I kan tilføje flere selv, kan sagtens vise hvordan i tilføjer, det går relativt nemt. - Profile repo er lavet. Lige nu har primært fokuseret på at kunne styre en bruger, i henhold til CRUD. Jeg har også givet forskellige permissions, f.eks. owner kan oprette leder og medarbejder. Leder kan kun oprette medarbejder. - Dette er selvfølgelig også ført videre til service og controller. - Mangler test :)

--- src/main/java/com/example/betasolutions/controller/ProfileController.java
+++ src/main/java/com/example/betasolutions/controller/ProfileController.java
@@ -1,8 +1,10 @@
 package com.example.betasolutions.controller;
 
+import com.example.betasolutions.enums.Role;
 import com.example.betasolutions.model.Profile;
 import com.example.betasolutions.service.ProfileService;
 import com.example.betasolutions.service.SkillService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,37 +23,96 @@ public class ProfileController {
     }
 
     @GetMapping("/profiles")
-    public String getAllProfiles(Model model){
+    public String getAllProfiles(Model model, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
 
+        model.addAttribute("profiles", profileService.getAllProfiles());
+        return "profiles/index";
     }
 
     @GetMapping("/profiles/{id}")
-    public String getProfileById(@PathVariable int id, Model model){
+    public String getProfileById(@PathVariable int id, Model model, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
 
+        model.addAttribute("profile", profileService.getProfileById(id));
+        return "profiles/profile";
     }
 
     @GetMapping("/profiles/create")
-    public String showCreateForm(Model model){
-
+    public String showCreateForm(Model model, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
+        model.addAttribute("profileForm", new Profile());
+        model.addAttribute("isOwner", isOwner(httpSession));
+        return "profiles/form";
     }
 
     @PostMapping("/profiles/create")
-    public String createProfile(@ModelAttribute Profile profile){
+    public String createProfile(@ModelAttribute Profile profile, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
 
+        if(profile.getRole() == Role.PROJECT_MANAGER && !isOwner(httpSession)){
+            return "redirect:/unauthorized";
+        }
+
+        profileService.createProfile(profile);
+        return "redirect:/profiles";
     }
 
     @GetMapping("/profiles/{id}/edit")
-    public String showEditForm(@PathVariable int id, Model model){
-
+    public String showEditForm(@PathVariable int id, Model model, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
+        model.addAttribute("profileEdit", profileService.getProfileById(id));
+        return "profiles/edit";
     }
 
     @PostMapping("/profiles/{id}/edit")
-    public String updateProfile(@PathVariable int id, @ModelAttribute Profile profile){
-
+    public String updateProfile(@PathVariable int id, @ModelAttribute Profile profile, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
+        profileService.updateProfile(profile, id);
+        return "redirect:/profiles";
     }
 
     @PostMapping("/profiles/{id}/delete")
-    public String deleteProfile(@PathVariable int id){
+    public String deleteProfile(@PathVariable int id, HttpSession httpSession){
+        if (!hasProfileAccess(httpSession)) {
+            return "redirect:/unauthorized";
+        }
 
+        Profile profileToDelete = profileService.getProfileById(id);
+
+        if(profileToDelete.getRole() == Role.PROJECT_MANAGER && !isOwner(httpSession)){
+            return "redirect:/unauthorized";
+        }
+
+        profileService.deleteProfile(id);
+        return "redirect:/profiles";
+    }
+
+    //Hjælpemetoder
+
+    private boolean hasProfileAccess(HttpSession httpSession) {
+        Profile loggedIn = (Profile) httpSession.getAttribute("profile");
+        return loggedIn != null && loggedIn.getRole() != Role.DEVELOPER;
+    }
+
+    private Profile getLoggedIn(HttpSession httpSession) {
+        return (Profile) httpSession.getAttribute("profile");
+    }
+
+    private boolean isOwner(HttpSession httpSession) {
+        Profile loggedIn = getLoggedIn(httpSession);
+        return loggedIn != null && loggedIn.getRole() == Role.OWNER;
     }
 }
